47f35b9f10f460bad9c5d0e29d4b0de2828097d0
Jongjun Park <lg2341@gmail.com>
2015-06-05 00:51:13 +0900

    액티비티 추가에 따른 매니페스트 파일 수정. 메인 엑티비티의 운동일지 버튼 추가.
    /*
    * 2015-06-05
    * 메인 액티비티에서 운동일지 버튼을 누르면 ExerciseLogListActivity를 호출하고
    * 인탠트에 list클래스를 전달한다.
    *
    * 앱이 실행될 때 저장되어 있는 list값을 불러와야한다. 없으면 생성.
    * 종료될 때 list를 저장한다.
    *
    * 사용자 생성 버튼에서 입력받은 이름과 첫 체중에서 첫 체중도 ExerciseLogListActivity를 호출 할 때
    * 같이 넣어야한다.
    *
    * */

--- app/src/main/java/com/example/jongjun/healthcare/MainActivity.java
+++ app/src/main/java/com/example/jongjun/healthcare/MainActivity.java
@@ -1,17 +1,51 @@
 package com.example.jongjun.healthcare;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import java.util.ArrayList;
+
+/*
+* 2015-06-05
+* 메인 액티비티에서 운동일지 버튼을 누르면 ExerciseLogListActivity를 호출하고
+* 인탠트에 list클래스를 전달한다.
+*
+* 앱이 실행될 때 저장되어 있는 list값을 불러와야한다. 없으면 생성.
+* 종료될 때 list를 저장한다.
+*
+* 사용자 생성 버튼에서 입력받은 이름과 첫 체중에서 첫 체중도 ExerciseLogListActivity를 호출 할 때
+* 같이 넣어야한다.
+*
+* */
 
 public class MainActivity extends ActionBarActivity {
+    static final int EXERCISE_VIEW=1;
+    Button button;
+    ArrayList<ExerciseLog> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.exerciseButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ExerciseLogListActivity.class);
+                intent.putParcelableArrayListExtra("LogList",list);
+                startActivityForResult(intent,EXERCISE_VIEW);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
     }
 
 
