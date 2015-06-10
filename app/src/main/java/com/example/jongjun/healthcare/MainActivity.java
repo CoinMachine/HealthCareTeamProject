cd46b8538e9363890a5cc24a57ea2598aa7df003
Jongjun Park <lg2341@gmail.com>
2015-06-10 20:03:05 +0900

    심플 만보계 기능 추가

--- app/src/main/java/com/example/jongjun/healthcare/MainActivity.java
+++ app/src/main/java/com/example/jongjun/healthcare/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends Activity {
     final String FILENAME1 = "user.dat";
     static final int EXERCISE_VIEW = 1;
     static final int USER_SET_VIEW = 2;
-    ImageButton exerciseButton, userButton, infoButton;
+    ImageButton exerciseButton, userButton, infoButton,counterButton;
     TextView name, weight;
     UserData userData;
 
@@ -60,6 +60,7 @@ public class MainActivity extends Activity {
         exerciseButton = (ImageButton) findViewById(R.id.exerciseButton);
         userButton = (ImageButton) findViewById(R.id.userButton);
         infoButton = (ImageButton) findViewById(R.id.infoButton);
+        counterButton=(ImageButton)findViewById(R.id.counterButton);
 
         try {
             FileInputStream fis = openFileInput(FILENAME1);
@@ -107,6 +108,14 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
+        counterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ShakeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
